2a542836179559a10bfae7c1d128e3632e7e861d
Yevhenii Didenko <ydidenko@f0r1s2.codam.nl>
2023-10-03 15:16:40 +0200

    init

---
+++ libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ydidenko <ydidenko@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 15:07:31 by ydidenko      #+#    #+#                 */
+/*   Updated: 2023/10/03 15:11:31 by ydidenko      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+#define true 1;
+#define false 0;
+
+#endif
